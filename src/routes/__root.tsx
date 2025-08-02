6ff5b69030a350af484fb33ff730f3aae35445cb
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-08-02 19:21:06 +0200

    feat: update meta tags, favicon, and add head content to routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,24 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: 'description',
+        content:
+          'Share, explore and build on the best startup ideas and side hustles',
+      },
+      {
+        title: 'IdeaDrop - Your Idea Hub',
+      },
+    ],
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
