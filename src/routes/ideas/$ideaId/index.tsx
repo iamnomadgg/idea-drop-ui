55482763853ac25dc207e4b448613b3483556248
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-08-03 20:55:02 +0200

    feat: implement idea details page with data fetching by ideaId

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+const fetchIdea = async (ideaId: string) => {
+  const res = await fetch(`http://localhost:8000/ideas/${ideaId}`);
+  if (!res.ok) throw new Error('failed to fetch data');
+  const data = await res.json();
+  return data;
+};
+
 export const Route = createFileRoute('/ideas/$ideaId/')({
   component: IdeaDetailsPage,
+  loader: async ({ params }) => {
+    return fetchIdea(params.ideaId);
+  },
 });
 
 function IdeaDetailsPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>;
+  const idea = Route.useLoaderData();
+  return <div>{idea.title}</div>;
 }
