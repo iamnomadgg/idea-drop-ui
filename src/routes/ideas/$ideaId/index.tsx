e9b92b355ff3701d7304f70a19ed4222ff809dd2
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-08-04 15:04:03 +0200

    feat: add idea deletion with API and button on details page

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,6 +1,10 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
-import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
-import { fetchIdea } from '@/api/ideas';
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+import {
+  queryOptions,
+  useSuspenseQuery,
+  useMutation,
+} from '@tanstack/react-query';
+import { fetchIdea, deleteIdea } from '@/api/ideas';
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
@@ -18,6 +22,26 @@ export const Route = createFileRoute('/ideas/$ideaId/')({
 function IdeaDetailsPage() {
   const { ideaId } = Route.useParams();
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
+
+  const navigate = useNavigate();
+  const { mutateAsync: deleteMutate, isPending } = useMutation({
+    mutationFn: () => deleteIdea(ideaId),
+    onSuccess: () => {
+      navigate({
+        to: '/ideas',
+      });
+    },
+  });
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm(
+      'Are you you want to delete this idea?'
+    );
+    if (confirmDelete) {
+      await deleteMutate();
+    }
+  };
+
   return (
     <div className="p-4">
       <Link to="/ideas" className="text-blue-500 underline block mb-4">
@@ -25,6 +49,13 @@ function IdeaDetailsPage() {
       </Link>
       <h2 className="text-2xl font-bold">{idea.title}</h2>
       <p className="mt-2">{idea.description}</p>
+      <button
+        onClick={handleDelete}
+        disabled={isPending}
+        className="text-sm bg-red-600 hover:bg-red-700 text-white mt-4 px-4 py-2 rounded transition disabled:opacity-50"
+      >
+        {isPending ? 'Deleting...' : 'Delete'}
+      </button>
     </div>
   );
 }
